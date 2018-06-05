b3db9c656cc5ed3d090bbc06af72688bb86b8e36
Amin <muhammad.amin1@afiniti.com>
2018-06-06 01:03:11 +0500

    Added delete todos by id route

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,6 +3,17 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+//Todo.findOneAndRemove().then();
+Todo.findByIdAndRemove('5b13f698cd3a573e986b8627').then((todo) => {
+    console.log(todo);
+}, (err) => {
+    console.log(err);
+});
 // var id = '5b13f698cd3a573e986b86271';
 // if(!ObjectID.isValid(id)){
 //     console.log('Id not valid');
--- server/server.js
+++ server/server.js
@@ -50,6 +50,27 @@ app.get('/todos/:id', (req, res) => {
     }
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    else{
+        Todo.findByIdAndRemove({
+            _id: id
+        }).then((todo) => {
+            if(todo){
+                res.status(200).send(todo);
+            }
+            else{
+                res.status(404).send();
+            }
+        }, (err) => {
+            res.status(400).send();
+        });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
